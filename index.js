43bbe41f0dfb9f0669801da76b163e416baecd93
bmwecker <141860318+bmwecker@users.noreply.github.com>
2025-02-13 09:17:07 +0100

    Исправлен импорт HeyGen SDK

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import HeygenStreamingAvatar from "@heygen/streaming-avatar"; // <-- исправленный импорт
+import * as HeygenStreamingAvatar from "@heygen/streaming-avatar"; // Исправленный импорт
 
 dotenv.config(); // Загружаем переменные из .env
 
