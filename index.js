7fd542cc14ec5497035b28ba5c5e7220dfae4a52
bmwecker <141860318+bmwecker@users.noreply.github.com>
2025-02-13 09:26:39 +0100

    Исправлена поддержка CommonJS для HeyGen SDK

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import * as HeygenStreamingAvatar from "@heygen/streaming-avatar"; // Исправленный импорт
+const HeygenStreamingAvatar = require("@heygen/streaming-avatar"); // Исправленный импорт
 
 dotenv.config(); // Загружаем переменные из .env
 
